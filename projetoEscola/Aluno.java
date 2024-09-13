945bd0ce1ae67de49b432c93f89f58227f340ae7
AlesSandro Silva <101981656+AlessandroDaCostaESilva@users.noreply.github.com>
2024-09-13 12:25:50 +0000

    Escola

---
+++ projetoEscola/Aluno.java
@@ -0,0 +1,10 @@
+package projetoEscola;
+
+public class Aluno {
+    public String nome;
+    public int matricula;
+
+    public void realizarCadastro(){
+        System.out.println("Realizando cadastro...");
+    }
+}
